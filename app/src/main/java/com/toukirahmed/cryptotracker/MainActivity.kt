2af6b7d61843e2f53f0718709827b5cfad3622ba
Toukir Ahemed <toukir.ahemd@dmoneybd.local>
2025-01-08 18:54:29 +0600

    Handling Network Errors complete

--- app/src/main/java/com/toukirahmed/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/toukirahmed/cryptotracker/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.toukirahmed.cryptotracker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.toukirahmed.cryptotracker.core.presentation.util.toString
+import com.toukirahmed.cryptotracker.core.presentation.util.ObserveAsEvents
+import com.toukirahmed.cryptotracker.crypto.presentation.coin_list.CoinListEvent
 import com.toukirahmed.cryptotracker.crypto.presentation.coin_list.CoinListScreen
+import com.toukirahmed.cryptotracker.crypto.presentation.coin_list.CoinListState
 import com.toukirahmed.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
+import com.toukirahmed.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.toukirahmed.cryptotracker.ui.theme.CryptoTrackerTheme
 import org.koin.androidx.compose.koinViewModel
 
@@ -27,6 +37,18 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val viewModel = koinViewModel<CoinListViewModel>()
                     val state by viewModel.state.collectAsStateWithLifecycle()
+                    val context = LocalContext.current
+                    ObserveAsEvents(events = viewModel.events) { event ->
+                        when(event) {
+                            is CoinListEvent.Error -> {
+                                Toast.makeText(
+                                    context,
+                                    event.error.toString(context),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    }
                     CoinListScreen(
                         state = state,
                         modifier = Modifier.padding(innerPadding)
@@ -34,5 +56,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinListScreenPreview() {
+    CryptoTrackerTheme {
+        CoinListScreen(
+            state = CoinListState(
+                coins = (1 .. 100).map {
+                    previewCoin.copy(id = it.toString())
+                }
+            ),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+        )
     }
 }
